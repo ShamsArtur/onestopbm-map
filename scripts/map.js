953004d125951a444b35ebe89243aa2706187b4e
Artur Shamsiyarov <shamsiyarov.artur@yandex.ru>
2024-01-27 21:52:28 +0500

    first working version

--- scripts/map.js
+++ scripts/map.js
@@ -1,13 +1,10 @@
 $(window).on('load', function() {
+
+    var map;
     
     loadMap();
     
     function loadMap(){
-        var objects = getObjects();
-        if (objects.length === 0){
-            alert('NO DATA FOUND');
-            return;
-        }
 
         document.title = 'ONE STOP BM';
 
@@ -34,10 +31,11 @@ $(window).on('load', function() {
               }
           });
   
-          const map = L.map("map", config).setView([lat, lng], zoom);
+          map = L.map("map", config).setView([lat, lng], zoom);
+
           L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
           
-          var geojsonLayer = new L.geoJson("..\geodata\areas.geojsonn", {
+          var geojsonLayer = new L.geoJson.ajax("https://raw.githubusercontent.com/ShamsArtur/onestop-geodata-areas/main/areas.geojson", {
 			pointToLayer: function(feature, latlng) {
 				return new L.marker(latlng, {icon: shopIcon});
 			},
@@ -69,14 +67,22 @@ $(window).on('load', function() {
 			},'Zoom To Home').addTo(map);
 
 		L.Control.geocoder().addTo(map);
-		// L.marker([lat, lng], {icon: greenIcon}).addTo(map);
 
+        getObjects();
 
     }
 
+    function getDate(dateString){
+        let text = dateString.toString();
+        let d = text.split(".");
+        let dat = new Date(d[2] + '/' + d[1] + '/' + d[0]);
+        return dat;
+    }
+
+    
     function getObjects(){
 
-        var objects = {};
+        var mapObjects = {};
 
         var googleDocURL = 'https://docs.google.com/spreadsheets/d/11wgKZMzyoEW9rUWKGQOeapYrrGx5skCs2aPaqquhPzE/edit';
         var googleApiKey = 'AIzaSyD97t4JrSFYVxJvuZgsuBiVWBzLmDBXQIQ';
@@ -92,26 +98,22 @@ $(window).on('load', function() {
 
         var getCoordinates = function(query){
             var value = {};
-            var url = "https://nominatim.openstreetmap.org/search.php?q=" + query + "&format=jsonv2"
-            $.getJSON(
-                url
-            ).then(function(data){
-                if (data[0]){
-                    value.lat = data[0].lat;
-                    value.lon = data[0].lon;
+            var url = "https://nominatim.openstreetmap.org/search.php?q=" + query + "&format=jsonv2";
+
+            $.ajax({
+                dataType: "json",
+                async: false,
+                url: url,
+                success: function(data){
+                    if (data[0]){
+                        value.lat = data[0].lat;
+                        value.lon = data[0].lon;
+                    }
                 }
-            })
+            });
             return value;
         }
 
-        var getDate = function(dateString){
-            let text = dateString;
-            let d = text.split(".");
-            let dat = new Date(d[2] + '/' + d[1] + '/' + d[0]);
-            return dat;
-        }
-
-        
         $.getJSON(
             apiUrl + spreadsheetId + '?key=' + googleApiKey
         ).then(function(data){
@@ -119,31 +121,73 @@ $(window).on('load', function() {
             if (sheets.length === 0 || !sheets.includes('Sheet1')){
                 'No data found'
             }
-
+    
             $.when(
                 $.getJSON(apiUrl + spreadsheetId + '/values/Sheet1?key=' + googleApiKey)
             ).done(function(data){
-                const result = [...parse([data]).reduce((r, o) => {
-                    const key = o["DELIVERY ADDRESS"] + '-' + o["ACCOUNT RFFERENCE"]
-                    
-                    const item = r.get(key) || Object.assign({}, o, {
-                        "SUM": 0,
-                        "COORDINATES": {},
-                        "DATE": "01.01.0001"
-                    });
-                    
-                    item["SUM"] += parseFloat(o["SUM OF INVOICE"]);
-                    item["COORDINATES"] = getCoordinates(o["POST CODE"] + " " + o["DELIVERY ADDRESS"]);
-                    if (getDate(item["INVOICE DATE"]) > getDate(item["DATE"]))
-                        item["DATE"] = item["INVOICE DATE"];
-                
-                    return r.set(key, item);
-                }, new Map).values()];
-                
-                objects = result;
-            })
 
+                var parsedData = parse([data]);
+                var hash = new Map();
+
+                parsedData.forEach(function(item){
+                    hashItem = {};
+                    var key = item["DELIVERY ADDRESS"] + '-' + item["ACCOUNT REFERENCE"];
+                    if (hash.has(key)){
+                        hashItem = hash.get(key);
+                    } else{
+                        hashItem.name = item["POST CODE"] + ' ' + item["DELIVERY ADDRESS"];
+                        hashItem.customer = item["ACCOUNT REFERENCE"];
+                        hashItem.date = "01.01.0001"
+                        hashItem.coordinates = getCoordinates(item["POST CODE"] + " " + item["DELIVERY ADDRESS"]);
+                        hashItem.sum = 0;
+                    }
+
+                    hashItem.sum += parseFloat(item["SUM OF INVOICE"]);
+
+                    if (getDate(item["INVOICE DATE"]) > getDate(hashItem.date))
+                        hashItem.date = item["INVOICE DATE"]
+
+                    hash.set(key, hashItem);
+                });
+
+                console.log(hash);
+                hash.forEach(function(item){
+                    console.log(item);
+                    addMarkerToLayer(item);
+                });
+            });
+        })
+    };
+
+
+    function addMarkerToLayer(item){
+        const redMarker = L.AwesomeMarkers.icon({
+            icon: 'star',
+            markerColor: 'red'
         });
-        return objects;
+        
+        const greenMarker = L.AwesomeMarkers.icon({
+            icon: 'star',
+            markerColor: 'green'
+        });
+
+        const dayinmsseconds = 86400000;
+        var today = new Date();
+        const diffDays = Math.ceil(Math.abs(today - getDate(item.date)) / (dayinmsseconds)); 
+
+        const markerIcon = (diffDays < 7) ? greenMarker : redMarker;
+
+        var popupText = '<pre><h2>' + item.name;
+        popupText += '<br>Customer:' + item.customer;
+        popupText += '<br>Last Invoice Date: ' + item.date;
+        popupText += '<br>Sum of invoices: ' + item.sum;
+        var popup = L.popup()
+            .setContent(popupText);
+        
+        var marker = L.marker(item.coordinates, { icon: markerIcon }).addTo(map).bindPopup(popup);
+
     }
+
 });
+
+
